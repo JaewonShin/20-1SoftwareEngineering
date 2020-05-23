52e43ed022d3df4c6e02d024130f1bd447f8741a
JaewonShin <49338879+JaewonShin@users.noreply.github.com>
2020-05-23 21:45:09 +0900

    메인 GUI 수정

--- Tetris_Main.c
+++ Tetris_Main.c
@@ -8,37 +8,210 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <time.h>
-#include "Tetris_GameStart.h"
+#include<locale.h>
+#include<ncursesw/curses.h>
+#include "GameStart2P.h"
+#include "GameStart.h"
+
 
 
 int game = GAME_END; /*게임 상태 변수, 게임이 시작되거나 종료됨에 따라 변한다*/
+char i_block[4][4][4] =
+	{
+			{0, 0, 0, 0,
+			 0, 0, 0, 0,
+			 1, 1, 1, 1,
+			 0, 0, 0, 0},
 
+			{0, 0, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 1, 0},
+
+			{0, 0, 0, 0,
+			 0, 0, 0, 0,
+			 1, 1, 1, 1,   
+			 0, 0, 0, 0},
+
+			{0, 0, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 1, 0}
+	};
+
+char t_block[4][4][4] =
+
+	{
+			{0, 1, 0, 0,
+			 0, 1, 1, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 0, 0, 0,
+			 1, 1, 1, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 1, 0, 0,
+			 1, 1, 1, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0}
+	};
+
+char s_block[4][4][4] =
+	{
+			{1, 0, 0, 0,
+			 1, 1, 0, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 1, 1, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 1, 0, 0,
+			 0, 1, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 0, 0},
+
+			{0, 0, 0, 0,
+			 0, 1, 1, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0}
+	};
+
+char z_block[4][4][4] =
+	{
+			{0, 1, 0, 0,
+			 1, 1, 0, 0,
+			 1, 0, 0, 0,
+			 0, 0,0, 0},
+
+			{1, 1, 0, 0,
+			 0, 1, 1, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 0, 1, 0,
+			 0, 1, 1, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 0, 0, 0,
+			 1, 1, 0, 0,
+			 0, 1, 1, 0,
+			 0, 0, 0, 0}
+	};
+
+char l_block[4][4][4] =
+	{
+			{0,1, 0, 0, 
+			 0,1, 0, 0, 
+			 0,1, 1, 0, 
+			 0,0, 0, 0},
+
+			{0, 0, 0, 0,
+			 1, 1, 1, 0,
+			 1, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 1, 0,0,
+			 0, 1, 0,0,
+			 0, 1, 0,0,
+			 0, 0, 0,0},
+
+			{0, 0, 1, 0,
+			 1, 1, 1, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0}
+	};
+
+char j_block[4][4][4] =
+	{
+			{0, 1, 0, 0,
+			 0, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 0, 0, 0,
+			 1, 1, 1, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{0,1, 1, 0, 
+			 0,1, 0, 0, 
+			 0,1, 0, 0, 
+			 0,0, 0, 0},
+
+			{0, 0, 0, 0,
+			 1, 1, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 0, 0}
+	};
+
+char o_block[4][4][4] =
+	{
+			{1, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0}
+	};
 int display_menu(void); /* 메뉴를 보여줌 */
+void tetris_plate(void);/*게임 시작 시 블럭모양 TETRIS 글자를 그려주는 함수*/
+
 
 int main(void)
 {
 	int menu = 1;
+	setlocale(LC_CTYPE, "ko_KR.utf-8");
 
-	while(1)
+	while(menu)
 	{
 		menu = display_menu();
 
 		if(menu == 1)
 		{
 			game = GAME_START;
-			game_start();
+			menu = game_start();
 		}
 		else if(menu == 2)
 		{
-			search_result();
-		}
-		else if(menu == 3)
-		{
-			print_result();
+			game = GAME_START;
+			menu = game_start_2p();
 		}
 		else if(menu == 4)
 		{
-			system("clear");
+			search_result();
+		}
+		else if(menu == 5)
+		{
+			print_result();
+		}
+		else if(menu == 6)
+		{
+			clear();
+			endwin();
 			exit(0);
 		}
 	}
@@ -50,9 +223,10 @@ int main(void)
 int display_menu(void)
 {
 	int menu = 0;
-
+	curs_set(1);
 	while(1)
 	{
+		
 		initscr();
 		clear();
 		move(3,10);
@@ -67,7 +241,7 @@ int display_menu(void)
 		move(5,10);
 		addch(ACS_VLINE);
 		move(5,0);
-		tetris2(); //네모칸사이에 TETRIS 그림 그려주기 
+		tetris_plate(); //네모칸사이에 TETRIS 그림 그려주기 
 		move(5,59);
 		addch(ACS_VLINE);
 		move(6,10);
@@ -127,18 +301,83 @@ int display_menu(void)
 	
 	
 
-		scanf("%d",&menu);
-		if(menu < 1 || menu > 4)
+		scanw("%d",&menu);
+		if(menu != 4) /*검색시에만 커서 살려두기*/
+			curs_set(0);
+		if(menu < 1 || menu > 6)
 		{
+			endwin();
 			continue;
 		}
 		else
 		{
+			endwin();
 			return menu;
 		}
 	}
 	return 0;
 }
+
+/*문자를 바로 입력 받을 수 있는 함수*/
+int _getch(void)
+{
+             char   ch;
+             int   error;
+             static struct termios Otty, Ntty;
+
+             fflush(stdout);
+             tcgetattr(0, &Otty);
+             Ntty = Otty;
+             Ntty.c_iflag  =  0;
+             Ntty.c_oflag  =  0;
+             Ntty.c_lflag &= ~ICANON;
+#if 1
+            Ntty.c_lflag &= ~ECHO;
+#else
+            Ntty.c_lflag |=  ECHO;
+#endif
+            Ntty.c_cc[VMIN]  = CCHAR;
+            Ntty.c_cc[VTIME] = CTIME;
+
+#if 1
+#define FLAG TCSAFLUSH
+#else
+#define FLAG TCSANOW
+#endif
+
+            if (0 == (error = tcsetattr(0, FLAG, &Ntty)))
+            {
+                       error  = read(0, &ch, 1 );
+                       error += tcsetattr(0, FLAG, &Otty);
+            }
+
+            return (error == 1 ? (int) ch : -1 );
+}
+
+
+int set_ticker(int game_state)
+{
+        struct itimerval new_timeset;
+	
+	if(game_state==GAME_START){
+		new_timeset.it_interval.tv_sec  = 0;        /* set reload  */
+		new_timeset.it_interval.tv_usec = 1;      /* new ticker value */
+		new_timeset.it_value.tv_sec     = 0  ;      /* store this   */
+		new_timeset.it_value.tv_usec    = 1 ;     /* and this     */
+	}
+	else if(game_state==GAME_END){
+		new_timeset.it_interval.tv_sec  = 0;        /* set reload  */
+		new_timeset.it_interval.tv_usec = 0;      /* new ticker value */
+		new_timeset.it_value.tv_sec     = 0  ;      /* store this   */
+		new_timeset.it_value.tv_usec    = 0 ;     /* and this     */
+	}
+	else return 1;
+
+	return setitimer(ITIMER_VIRTUAL, &new_timeset, NULL);
+}
+
+
+///
 
 void tetris_plate(void) //칸에 맞춰 출력하기 위한 메인화면 TETRIS 그림 
 {
