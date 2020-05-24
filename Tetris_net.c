d8e074084418668a35c0610f73fc5d1b980d72d7
JojiFrank <63050540+JojiFrank@users.noreply.github.com>
2020-05-24 23:46:32 +0900

    Update Tetris_net.c

--- Tetris_net.c
+++ Tetris_net.c
@@ -403,69 +403,6 @@ int game_start_net(void)
   return 0;
 }
 
-void server(void)
-{
-	host_net = 1;
-	listen_sock = socket(PF_INET, SOCK_STREAM, 0);
-	memset(&listen_addr, 0, sizeof(listen_addr));
-	listen_addr.sin_family = AF_INET;
-	listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	listen_addr.sin_port = htons(PORT);
-	
-	if(bind(listen_sock, (struct sockaddr*)&listen_addr, sizeof(listen_addr)) < 0) {
-		printw("ERROR_BIND FAILED");
-		return;
-	}
-	if(listen(listen_sock,1) < 0) {	
-		printf("ERROR_소켓 리슨 실패\n");
-		return;
-	}
-
-	int sockaddr_in_size = sizeof(connect_addr);
-	
-	server_sock = accept(listen_sock, (struct sockaddr*)&connect_addr,
-	&sockaddr_in_size);	// 소켓연결이 들어올때까지 대기
-
-	char msg[1024] = { 0 };	
-
-	menu = game_start_net();
-	flag =1;
-	send(server_sock,(char*)&flag,sizeof(int),0); //플래그값 클라이언트에게 쏴줌 
-	lose_game == losemsg; //함수 통과시 게임 패배로 판단 
-	close(server_sock);	
-	close(listen_sock);
-}
-
-void client(void)
-{
-	host_net = 2;
-	client_sock = socket(PF_INET, SOCK_STREAM, 0);
-	clear();
-	move(11,23);	
-	printw("서버의 아이피주소를 입력하세요: ");
-	scanw("%s", &ip);
-
-	memset(&client_addr, 0, sizeof(client_addr));
-	client_addr.sin_family = AF_INET;
-	client_addr.sin_addr.s_addr = inet_addr(ip);
-	client_addr.sin_port = htons(PORT);
-	
-	if (connect(client_sock, (struct sockaddr*)&client_addr, sizeof(client_addr)) < 0) { //입력한 주소로 접속
-		printw("접속 실패");
-		close(client_sock);
-		return;
-	}
-	
-	menu = game_start_net();
-	lose_game == losemsg; //함수 먼저 통과했으니 패배한것  
-	flag = 1; //함수를 통과했으므로 flag 1값으로 변경후 서버에게 쏴줌으로 게임 종료를 알림 .
-	send(client_sock,(char*)&flag, sizeof(int), 0);
-	
-	close(client_sock);	// 소켓을 닫는다.
-
-	
-}
-
 void tetris2(void) //칸에 맞춰 출력하기 위한 메인화면 TETRIS 그림 
 {
 	move(4,14);
