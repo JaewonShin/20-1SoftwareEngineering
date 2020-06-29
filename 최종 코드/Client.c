459a939d590cd4772f769a948bb11a6cee8bff7a
JaewonShin <49338879+JaewonShin@users.noreply.github.com>
2020-06-29 23:53:24 +0900

    테스트 오류 수정

--- 최종 코드/Client.c
+++ 최종 코드/Client.c
@@ -13,29 +13,37 @@
 
 void client(void)
 {
+	clear();
 	host_net = 2;
 
 	// socket()
 	client_sock = socket(PF_INET, SOCK_STREAM, 0);
-	clear();
-	// get IP adress	
-	move(11, 23);
-	printw("서버의 아이피주소를 입력하세요 : ");
-	scanw("%s", &ip);
+	
+	// get IP adress
+	while(1) {	
+		move(11, 23);
+		printw("서버의 아이피주소를 입력하세요 : ");
+		scanw("%s", &ip);
+		
+		// adress setting
+		memset(&client_addr, 0, sizeof(struct sockaddr_in));
+		client_addr.sin_family = AF_INET;
+		client_addr.sin_addr.s_addr = inet_addr(ip);
+		client_addr.sin_port = htons(23456); // temporary port num
 
-	// adress setting
-	memset(&client_addr, 0, sizeof(struct sockaddr_in));
-	client_addr.sin_family = AF_INET;
-	client_addr.sin_addr.s_addr = inet_addr(ip);
-	client_addr.sin_port = htons(23456); // temporary port num
+		// connect()
+		
+		if (connect(client_sock, (struct sockaddr*) & client_addr, sizeof(client_addr)) != 0) { //입력한 주소로 접속
+			clear();
+			move(10, 21);			
+			printw("연결에 실패하였습니다.");
+			move(11, 19);			
+			printw("다시");
+			continue;
+		}
+		else break;
 
-	// connect()
-	if (connect(client_sock, (struct sockaddr*) & client_addr, sizeof(client_addr)) < 0) { //입력한 주소로 접속
-		printw("접속 실패");
-		close(client_sock);
-		return;
 	}
-
 	// start game as client	
 	game_start_net();
 
